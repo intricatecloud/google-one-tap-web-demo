6d3526d8a184688037c1137378c0cd012d808ce3
Danny Perez <danny@intricatecloud.io>
2020-11-29 20:02:44 -0500

    React app with google one tap library loading

---
+++ src/App.js
@@ -0,0 +1,63 @@
+/* global google */
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react'
+import jwt_decode from 'jwt-decode';
+
+function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState(null)
+
+  const onOneTapSignedIn = response => {
+    setIsSignedIn(true)
+    const decodedToken = jwt_decode(response.credential)
+    setUserInfo({...decodedToken})
+  }
+
+  const initializeGSI = () => {
+    google.accounts.id.initialize({
+      // TODO - remove
+      client_id: '1006632388683-u8e68serndr8gfoukbgn6cddds94op5q.apps.googleusercontent.com',
+      cancel_on_tap_outside: false,
+      callback: onOneTapSignedIn
+    });
+    google.accounts.id.prompt((notification) => {
+      if (notification.isNotDisplayed()) {
+        console.log(notification.getNotDisplayedReason())
+      } else if (notification.isSkippedMoment()) {
+        console.log(notification.getSkippedReason())
+      } else if(notification.isDismissedMoment()) {
+        console.log(notification.getDismissedReason())
+      }
+    });
+  }
+
+  const signout = () => {
+    // refresh the page
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    const el = document.createElement('script')
+    el.setAttribute('src', 'https://accounts.google.com/gsi/client')
+    el.onload = () => initializeGSI();
+    document.querySelector('body').appendChild(el)
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        { isSignedIn ?
+          <div>
+            Hello {userInfo.name} ({userInfo.email})
+            <div className="g_id_signout" onClick={() => signout()}>Sign Out</div>
+          </div> :
+          <div>You are not signed in</div>
+        }
+      </header>
+    </div>
+  );
+}
+
+export default App;
